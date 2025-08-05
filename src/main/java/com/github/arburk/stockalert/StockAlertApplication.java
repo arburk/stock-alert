7a07e22ffe3f80775ed12151000310fca132711d
Arno Burkhart <arburk@users.noreply.github.com>
2025-08-05 13:41:55 +0200

    chore: initialized spring project

---
+++ src/main/java/com/github/arburk/stockalert/StockAlertApplication.java
@@ -0,0 +1,13 @@
+package com.github.arburk.stockalert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAlertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockAlertApplication.class, args);
+	}
+
+}
