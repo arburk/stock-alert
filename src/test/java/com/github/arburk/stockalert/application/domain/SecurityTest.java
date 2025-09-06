f85797d8f55944891009fe47e640e58ae7b0bca3
Arno Burkhart <arburk@users.noreply.github.com>
2025-09-06 11:50:30 +0200

    feat(#36): remove old logs of same alert type when new alert is raised

--- src/test/java/com/github/arburk/stockalert/application/domain/SecurityTest.java
+++ src/test/java/com/github/arburk/stockalert/application/domain/SecurityTest.java
@@ -3,6 +3,8 @@ package com.github.arburk.stockalert.application.domain;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -11,10 +13,11 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class SecurityTest {
 
+  private static final LocalDateTime NOW = LocalDateTime.now();
+
   @Test
   void addLogSameTypeNotDuplicated() {
-    final LocalDateTime now = LocalDateTime.now();
-    final LocalDateTime oneHorAgo = now.minusHours(1);
+    final LocalDateTime oneHorAgo = NOW.minusHours(1);
 
     final Security testee = new Security(null, null, null, null, null, null, null);
     assertTrue(testee.alertLog().isEmpty());
@@ -33,7 +36,7 @@ class SecurityTest {
     assertTrue(testee.alertLog().contains(alert1));
     assertTrue(testee.alertLog().contains(alert2));
 
-    final Alert copyOfAlert1 = new Alert(now, alert1.threshold(), alert1.unit());
+    final Alert copyOfAlert1 = new Alert(NOW, alert1.threshold(), alert1.unit());
     testee.addLog(copyOfAlert1);
     assertEquals(2, testee.alertLog().size());
     assertTrue(testee.alertLog().contains(copyOfAlert1));
@@ -42,5 +45,17 @@ class SecurityTest {
     final Alert result = testee.alertLog().stream().filter(log -> log.equals(alert1)).toList().getFirst();
     assertEquals(copyOfAlert1.timestamp(), result.timestamp());
     assertNotEquals(alert1.timestamp(), result.timestamp());
+  }
+
+  @Test
+  void oldAlertRemoved() {
+    final Alert alert1 = new Alert(NOW.minusHours(1), 1., "CHF");
+    final Alert alert2 = new Alert(NOW.minusHours(2), 1., "CHF");
+    final Alert mostRecent = new Alert(NOW, 1., "CHF");
+    final Security testee = new Security(null, null, null, null, null, null, new ArrayList<>(List.of(alert1, alert2)));
+
+    testee.addLog(mostRecent);
+    assertEquals(1, testee.alertLog().size());
+    assertEquals(NOW, testee.alertLog().stream().toList().getFirst().timestamp());
   }
 }
